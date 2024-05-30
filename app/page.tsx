fcaf58e69e5395a8a13b46966fe4ef65cf1fc94e
donavandaniels <Danielsdk99@hotmail.com>
2024-05-29 20:39:21 -0400

    fixed some css styles colors and center the div

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ export default function Home() {
   return (
     <div>
       <header className="font-bold text-3xl">
-      Why Over Spend Over Groceries? Use Savvy-Saver
+        <span className="block">Why Over Spend Over Groceries?</span>
+        <span className="block">Use Savvy-Saver</span>
       </header>
-        <button className="bg-black text-white font-bold rounded-full">
-          start shopping now
-        </button>
-    
+
+      <button className="bg-black text-white font-bold rounded-full">
+        start shopping now
+      </button>
     </div>
   );
 }
