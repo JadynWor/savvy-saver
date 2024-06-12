f7bf7db492964f2df85c87c232553c57b4c0b381
cthacker-udel <cthacker@udel.edu>
2024-06-12 16:45:03 -0400

    gl

--- src/app/logout/actions.ts
+++ src/app/logout/actions.ts
@@ -1,18 +1,19 @@
-'use server'
+"use server";
 
-import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+import { createClient } from "@/utils/supabase/server";
+
 export async function logout() {
-    const supabase = createClient()
-  
+    const supabase = createClient();
+
     const { error } = await supabase.auth.signOut();
-  
+
     if (error) {
-      redirect('/error')
+        redirect("/error");
     }
-  
-    revalidatePath('/', 'layout')
-    redirect('/')
-  }
+
+    revalidatePath("/", "layout");
+    redirect("/");
+}
