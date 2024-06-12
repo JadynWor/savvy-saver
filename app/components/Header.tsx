04314ac6ac99ee4b2a6befdfb9a1ebe33607a7d9
donavandaniels <Danielsdk99@hotmail.com>
2024-06-12 15:33:30 -0400

    fixed buttons and sign in page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -59,7 +59,6 @@ const Header = ({ children, session }: HeaderProperties) => {
                     </>
                 )}
             </nav>
-            {children}
         </header>
     );
 };
