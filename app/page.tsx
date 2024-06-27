3aa42e1aa085e38e54b3ee437125739ed005b1b3
donavandaniels <Danielsdk99@hotmail.com>
2024-06-27 15:47:35 -0400

    [donavan-branch]

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div className="bg-[(/home.jpg)] bg-cover flex flex-col h-full items-center justify-center pt-4">
-            <span className="font-bold text-3xl">
+        <div className="bg-[url(/home.jpg)] bg-cover flex flex-col h-full items-center justify-center pt-4">
+            <span className="font-bold text-3xl text-white ">
                 {" "}
                 Smart Savings, Closer to You
             </span>
@@ -14,6 +14,7 @@ export default function Home() {
                 <select className="w-32 p-2 border border-gray-300 rounded-md">
                     <option value="25">25 miles</option>
                     <option value="50">50 miles</option>
+                    <option value="75">75 miles</option>
                     <option value="100">100 miles</option>
                 </select>
                 <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
